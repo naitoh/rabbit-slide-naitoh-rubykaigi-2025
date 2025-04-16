4d15451b8bb68f8f390b46bbf9d323ec7b98c76d
NAITOH Jun <naitoh@gmail.com>
2025-04-17 03:00:24 +0900

    ワーニングメッセージ対策
    Co-authored-by: Sutou Kouhei <kou@clear-code.com>

--- theme.rb
+++ theme.rb
@@ -7,9 +7,8 @@ match TitleSlide do |slides|
 end
 include_theme("title-slide-background-image")
 
+@slide_logo_image_uninstall = true
 include_theme("clear-code")
-@slide_logo_image = ""
-include_theme("clear-blue")
 
 match(Slide, Table) do |e|
   e.prop_set "size", screen_size(2 * Pango::SCALE)
